ecbe50261dcc4fff45677cfc9d216d5acc33c8bd
franco <franko_millonarios@outlook.es>
2022-06-13 19:35:17 -0300

    TRABAJO

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,32 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+
+
+
+  return array = [1];
 }
+
+
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+
+   
+
+  return array[array.length - 1];
+
+
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+
+   return array.length;
 }
 
 
@@ -23,6 +37,8 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+
+  
 }
 
 
